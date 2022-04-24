75e615911264750b82fdebc7aa3e9cb2c94b18a2
Ranjit Kumar <ranjitkumar448@yahoo.com>
2022-04-25 03:56:16 +0530

    ..

--- src/server.js
+++ src/server.js
@@ -12,8 +12,7 @@ const io = new Server(server,
     methods: ["GET", "POST"],
     transports: ['websocket', 'polling'],
     credentials: true
-},
-    path: '/rgb/socket.io',
+  },
 
 allowEIO3: true});
 
